c77464a612df5682e9c2720fe26bd6450e015c76
St1ksy <gyngazov2004@mail.ru>
2025-02-22 15:30:00 +0700

    Имя класса изменено

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,16 +4,11 @@ from rest_framework import permissions
 # Класс для пользовательского разрешения, которое ограничивает доступ
 # к объектам, основываясь на том, является ли пользователь автором объекта
 # или запрос использует безопасный метод.
-class OwnershipPermission(permissions.BasePermission):
-
+class IsAuthorOrReadOnly(permissions.BasePermission):
     # Метод проверяет, есть ли у пользователя разрешение на выполнение запроса.
-    # Разрешение предоставляется, если метод запроса безопасный (GET, OPTIONS, HEAD)
-    # или если пользователь аутентифицирован.
+    # Разрешение предоставляется, если метод запроса безопасный (GET, OPTIONS, HEAD).
     def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+        return request.method in permissions.SAFE_METHODS
 
     # Метод проверяет разрешение на доступ к конкретному объекту.
     # Разрешение предоставляется, если метод запроса безопасный,
